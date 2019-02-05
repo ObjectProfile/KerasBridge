628ef6f4b21547feedb68eb9e8fedbd42e082785
Alejandro Infante <ainfante@dcc.uchile.cl>
2019-02-05 12:52:31 -0300

    Add progressbar.

--- pharo_keras_hooks.py
+++ pharo_keras_hooks.py
@@ -2,9 +2,9 @@ from PythonBridge import bridge_hooks
 import keras
 
 class Pharo_Fit_Notifier(keras.callbacks.Callback):
-    def __init__(self, notificationId):
+    def __init__(self, observer):
         super().__init__()
-        self.notificationId = notificationId
+        self.observer = observer
 
     def on_train_begin(self, logs={}):
         return
@@ -16,7 +16,7 @@ class Pharo_Fit_Notifier(keras.callbacks.Callback):
         return
  
     def on_epoch_end(self, epoch, logs={}):
-        pharo_hooks.notifyStep("", self.notificationId)
+        self.observer(None)
         return
  
     def on_batch_begin(self, batch, logs={}):
